24c0d09eb13d57ff821d29829ea5a32f05bdba5f
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2019-12-08 23:42:05 +0800

    Test orm success

--- Check_in_System/views.py
+++ Check_in_System/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import ClubMember
 
 def Index(request):
+    members = ClubMember.objects.all()
+    print(members)
     return render(request,'index.html')
 
 # Create your views here.
