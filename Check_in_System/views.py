5e7d522771ecd7a9b6dfa6166659c51f87d5c124
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2019-12-08 23:21:59 +0800

    Main page setup complete

--- Check_in_System/views.py
+++ Check_in_System/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+def Index(request):
+    return render(request,'index.html')
 
 # Create your views here.
--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Final_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
