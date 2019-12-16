25404e2097651bf6e239d592f85e1e059577d9f2
Maple30 <st0963105799@gmail.com>
2019-12-16 13:41:46 +0800

    Changinfo

--- Check_in_System/urls.py
+++ Check_in_System/urls.py
@@ -5,12 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.Index, name='index'),
     path('register/',views.register,name='register'),
-<<<<<<< HEAD
     path('Changeinfo/',views.Changeinfo,name='Changeinfo'),
-    
-=======
     path('activity/',views.activity,name='activity'),
     path('activity/show/<name>/',views.activityShow,name="activityShow"),
     path('activity/edit/',views.activityEdit,name="activityEdit")
->>>>>>> d90dcfd0b3af974df823de5ff128c6988c49a747
 ]
