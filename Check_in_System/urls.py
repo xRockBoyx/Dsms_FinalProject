d90dcfd0b3af974df823de5ff128c6988c49a747
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2019-12-16 11:11:06 +0800

    Admin Activity lookup adjust complete

--- Check_in_System/urls.py
+++ Check_in_System/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Index, name='index'),
-    path('register/',views.register,name='register')
+    path('register/',views.register,name='register'),
+    path('activity/',views.activity,name='activity'),
+    path('activity/show/<name>/',views.activityShow,name="activityShow"),
+    path('activity/edit/',views.activityEdit,name="activityEdit")
 ]
