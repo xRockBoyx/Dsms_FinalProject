4f6dba7c579d9089d57d22af6f80d161032c0d40
toyraynei <toyraynei@gmail.com>
2019-12-20 13:42:20 +0800

    我不知道發生什麼事惹...

--- Check_in_System/urls.py
+++ Check_in_System/urls.py
@@ -11,17 +11,8 @@ urlpatterns = [
     path('activity/edit/',views.activityEdit,name="activityEdit"),
     path('activity/delete/<name>/',views.activityDelete,name="activityDelete"),
     path('addactivity/',views.AddActivity,name="AddActivity"),
-<<<<<<< HEAD
-    path('CAL/',views.CAL,name="Club_Attend_List")
-
-=======
     path('checkin/<name>/',views.CheckInObjects,name = "CheckInObjects"),
     path('checkin/',views.CheckIn,name = "CheckIn"),
-<<<<<<< HEAD
     path('MemberManagement/',views.MemberManagement,name="MemberManagement"),
     path('CAL/',views.CAL,name = "CAL")
-=======
-    path('MemberManagement/',views.MemberManagement,name="MemberManagement")
->>>>>>> dacfd23fe1bdeb27d5561104e4aaca664112597a
->>>>>>> d7f4f2f039b59c8fde09b54b55570affe6e290b4
 ]
